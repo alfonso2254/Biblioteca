5e1fd23ce397d0e06616b9a3de827d4ff14816f9
alfonso2254 <45520382+alfonso2254@users.noreply.github.com>
2020-02-21 16:29:19 -0400

    modulos

---
+++ vistas/modulos/script.php
@@ -0,0 +1,10 @@
+	<script src="./js/jquery-3.1.1.min.js"></script>
+	<script src="./js/sweetalert2.min.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
+	<script src="./js/material.min.js"></script>
+	<script src="./js/ripples.min.js"></script>
+	<script src="./js/jquery.mCustomScrollbar.concat.min.js"></script>
+	<script src="./js/main.js"></script>
+	<script>
+		$.material.init();
+	</script>
---
+++ vistas/plantilla.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Inicio</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+	<!-- SideBar -->
+	<?php include "vistas/modulos/navlateral.php" ?>
+	<!-- Content page-->
+	<section class="full-box dashboard-contentPage">
+		<!-- NavBar -->
+	<?php include "vistas/modulos/navbar.php" ?>	
+		<!-- Content page -->
+
+
+
+	</section>
+
+	<!--====== Scripts -->
+	<?php include "vistas/modulos/sccript.php" ?>
+</body>
+</html>
